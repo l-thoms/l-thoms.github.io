9f8b631bc65187b96f90c0adec3668172da76b80
Thoms-L <2776285675@qq.com>
2020-08-10 10:05:02 +0800

    Change notice

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function load()
 {
 	request();
 	t = setTimeout(function(){
-		var d = document.getElementById("alertAnimation");
+		var d = document.getElementById("abortAnimation");
 		d.style.visibility="visible";
 		d.style.textAlign="Center";
 		d.style.position="unset";
@@ -25,18 +25,20 @@ function request()
 	if(url.indexOf("?")!=-1)
 	{
 		var argument = url.substring(1);
-		switch(argument)
+		switch(argument.toLowerCase())
 		{
-			case "Surf":self.location.href="https://surf.jackbuehner.com";break;
-			case "Fontviewer":self.location.href="https://github.com/l-thoms/Fontviewer/raw/master/Fontviewer/bin/x86/Release/Fontviewer.exe";break;
-			case "Fontviewer.wpf":self.location.href="https://github.com/l-thoms/Fontviewer/raw/master/Fontviewer.wpf/Fontviewer.wpf/bin/x86/Release/Fontviewer.wpf.exe";break;
-			case "Snake":self.location.href="./Snake.zip";break;
+			case "surf":window.location.href="https://surf.jackbuehner.com";break;
+			case "fontviewer":window.location.href="https://github.com/l-thoms/Fontviewer/raw/master/Fontviewer/bin/x86/Release/Fontviewer.exe";break;
+			case "fontviewer.wpf":window.location.href="https://github.com/l-thoms/Fontviewer/raw/master/Fontviewer.wpf/Fontviewer.wpf/bin/x86/Release/Fontviewer.wpf.exe";break;
+			case "snake":window.location.href="./Snake.zip";break;
+			case "yueping":
+			case "jyutping":window.location.href="http://www.iso10646hk.net/jp/database/index.jsp";break;
 		}
 	}
 }
 function resize()
 {
-	var r=document.getElementById('res');
+	var r=document.getElementById('res');//调整亚克力效果
 	var dw= document.documentElement.clientWidth.toString();
 	var dh= document.documentElement.clientHeight.toString();
 	applyCSS(".res::before{"+
@@ -46,13 +48,26 @@ function resize()
 	"bottom:-"+((dh-r.offsetHeight-r.offsetTop)/r.offsetHeight*100).toString()+"%;"+
 	"background-repeat: no-repeat;background-size: cover;background-attachment:fixed;background-position: center;}",
 	"autoSize",true);
+
+	var u = navigator.userAgent;
+	if(u.indexOf("Android") > -1||u.indexOf("iPhone") > -1)//适配手机页面缩放
+	{
+		if(window.orientation.toString()=="90"||window.orientation.toString()=="-90")
+		{
+			applyCSS("body{zoom:1;}","scaleForPhone",true);
+		}
+		else
+		{
+			applyCSS("body{zoom:2.5;}","scaleForPhone",true);
+		}
+	}
 	var f = document.getElementById("FFNotice");
 	var ff = document.getElementById("FFFooter");
 	//f.style.maxWidth=(document.documentElement.offsetWidth-
 	//document.getElementById("left_").clientWidth-
 	//document.getElementById("right_").clientWidth-12).toString()+"px";
 	
-	if(document.documentElement.offsetHeight<=document.documentElement.clientHeight)
+	if(document.documentElement.offsetHeight<=document.documentElement.clientHeight)//页面高度小于视图高度
 	{
 		ff.style.position = "fixed";
 		ff.style.width = document.documentElement.offsetWidth.toString()+"px";
@@ -67,23 +82,10 @@ function resize()
 	if(f.clientWidth>document.documentElement.offsetWidth-document.getElementById("left_").clientWidth-document.getElementById("right_").clientWidth)
 	{
 		ff.style.left = (document.getElementById("left_").clientWidth).toString()+"px";
-		ff.style.width = (document.documentElement.offsetWidth-document.getElementById("left_").clientWidth-document.getElementById("right_").clientWidth).toString()+"px";
-	}
-
-	var u = navigator.userAgent;
-	if(u.indexOf("Android") > -1||u.indexOf("iPhone") > -1)
-	{
-		if(window.orientation.toString()=="90"||window.orientation.toString()=="-90")
-		{
-			applyCSS("body{zoom:1;}","scaleForPhone",true);
-		}
-		else
-		{
-			applyCSS("body{zoom:2.5;}","scaleForPhone",true);
-		}
+		ff.style.width = (document.documentElement.offsetWidth-document.getElementById("left_").clientWidth-document.getElementById("right_").clientWidth-8).toString()+"px";
 	}
 }
-function applyCSS(t,id,isremoved)
+function applyCSS(t,id,isremoved)//添加CSS元素
 {
 	var s=document.createElement('style');
 	s.innerText=t;
@@ -99,7 +101,7 @@ function applyCSS(t,id,isremoved)
 	}
 	document.body.appendChild(s);
 }
-function ready()
+function ready()//文档状态
 {
 	if (document.readyState=="complete")
 	{
@@ -115,20 +117,7 @@ function ready()
 	}
 	r.addEventListener("animationend",function next()
 	{
-		var rc=document.getElementById('left');
-		rc.style.animationPlayState="running";
-		var rc=document.getElementById('left_');
-		rc.style.animationPlayState="running";
-		var rc=document.getElementById('right');
-		rc.style.animationPlayState="running";
-		var rc=document.getElementById('right_');
-		rc.style.animationPlayState="running";
-		var rc=document.getElementById('dyn');
-		rc.style.animationPlayState="running";
-		var rc=document.getElementById('dyn-content');
-		rc.style.animationPlayState="running";
-		var rc=document.getElementById('FFNotice');
-		rc.style.animationPlayState="running";
+		animationStopped();
 		var a = Math.ceil(Math.random()*100);
 		if(a>95) 
 		{
@@ -136,7 +125,24 @@ function ready()
 			i.style.display="inherit";
 			i.style.animationPlayState="running";
 		}
-		});
+	});
+}
+function animationStopped()//中止动画
+{
+	var rc=document.getElementById('left');
+	rc.style.animationPlayState="running";
+	var rc=document.getElementById('left_');
+	rc.style.animationPlayState="running";
+	var rc=document.getElementById('right');
+	rc.style.animationPlayState="running";
+	var rc=document.getElementById('right_');
+	rc.style.animationPlayState="running";
+	var rc=document.getElementById('dyn');
+	rc.style.animationPlayState="running";
+	var rc=document.getElementById('dyn-content');
+	rc.style.animationPlayState="running";
+	var rc=document.getElementById('FFNotice');
+	rc.style.animationPlayState="running";
 }
 function stopLoading()
 {
@@ -150,22 +156,9 @@ function stopLoading()
 	dyn.style.animationPlayState="running";
 	r.addEventListener("animationend",function next()
 	{
-		var r=document.getElementById('left');
-		r.style.animationPlayState="running";
-		var r=document.getElementById('left_');
-		r.style.animationPlayState="running";
-		var r=document.getElementById('right');
-		r.style.animationPlayState="running";
-		var r=document.getElementById('right_');
-		r.style.animationPlayState="running";
-		var r=document.getElementById('dyn');
-		r.style.animationPlayState="running";
-		var r=document.getElementById('dyn-content');
-		r.style.animationPlayState="running";
-		var r=document.getElementById('FFNotice');
-		r.style.animationPlayState="running";
+		animationStopped();//提前中止动画
 	});
-	var d = document.getElementById("alertAnimation");
+	var d = document.getElementById("aabortAnimation");
 	d.style.visibility="hidden";
 	d.style.position="fixed";
 }
